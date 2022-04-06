d12f71b26841abc201ebefe159b200c6f8ed4920
jklxco <jack.a.coleman@hotmail.com>
2022-04-06 19:10:45 +0700

    Added js functionality

--- scripts.js
+++ scripts.js
@@ -12,10 +12,36 @@ function operate(calcArray) {
         : (operator === '-') ? subtract(num1, num2)
         : (operator === '*') ? multiply(num1, num2)
         : (operator === '/') ? divide(num1, num2)
-        : 'undefined'; 
+        : 'undefined' ; 
 
 }
-
+/*
 const array = [13, "-", 6];
 console.log(operate(array));
+*/
 
+let display = document.querySelector('.display');
+const buttons = document.querySelectorAll(".button");
+let displayValue = '';
+display.textContent= 0;
+calcArray = [];
+
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', e => {
+        if (e.target.className === "button operator") {
+            calcArray.push(displayValue);
+            displayValue = '';
+            calcArray.push(e.toElement.id);
+        } else if (e.target.className === "button operator equals") {
+            calcArray.push(displayValue);
+            console.log(calcArray);
+            displayValue = operate(calcArray);
+            display.textContent = displayValue;
+            calcArray = [];
+            
+        } else {
+            displayValue += e.target.textContent;
+            display.textContent = displayValue;
+        };
+    });
+};
