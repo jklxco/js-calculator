0a39b6feb33b972f923037f1fdc0af76ecdc7227
jklxco <jack.a.coleman@hotmail.com>
2022-04-08 09:06:42 +0700

    Refactor calcArray for chaining equations

--- scripts.js
+++ scripts.js
@@ -5,22 +5,18 @@ divide = (a, b) => a / b;
 
 function operate(calcArray) {
     let operator = calcArray[1];
-    let num1 = calcArray[0];
-    let num2 = calcArray[2];
+    let num1 = Number( calcArray[0] );
+    let num2 = Number( calcArray[2] );
 
     return (operator === '+') ? add(num1, num2)
         : (operator === '-') ? subtract(num1, num2)
         : (operator === '*') ? multiply(num1, num2)
         : (operator === '/') ? divide(num1, num2)
-        : 'undefined' ; 
+        : 'undefined'; 
 
 }
-/*
-const array = [13, "-", 6];
-console.log(operate(array));
-*/
 
-let display = document.querySelector('.display');
+const display = document.querySelector('.display');
 const buttons = document.querySelectorAll(".button");
 let displayValue = '';
 display.textContent= 0;
@@ -28,17 +24,29 @@ calcArray = [];
 
 for (i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', e => {
-        if (e.target.className === "button operator") {
+
+        if (e.target.className.includes("button operator")) {
+
             calcArray.push(displayValue);
             displayValue = '';
-            calcArray.push(e.toElement.id);
-        } else if (e.target.className === "button operator equals") {
-            calcArray.push(displayValue);
-            console.log(calcArray);
-            displayValue = operate(calcArray);
-            display.textContent = displayValue;
+
+            if (calcArray.length < 3) {
+                calcArray.push(e.toElement.id);
+            } else {
+                displayValue = operate(calcArray);
+                display.textContent = displayValue;
+                calcArray = [];
+                calcArray.push(displayValue);
+                displayValue = '';
+                if (!e.target.className.includes('equals')) {
+                    calcArray.push(e.toElement.id);
+                };
+            };
+        } else if (e.target.className.includes("clear")) {
+            displayValue = [];
+            display.textContent = 0;
             calcArray = [];
-            
+
         } else {
             displayValue += e.target.textContent;
             display.textContent = displayValue;
